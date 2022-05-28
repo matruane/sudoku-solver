b4aa75555a565d49f7aa8bd6b49fd726f6b63061
Mat Ruane <8108538+matruane@users.noreply.github.com>
2022-05-28 20:40:53 +1200

    :test_tube: Add test for filling non-empty square

--- tests/fillSquare.test.js
+++ tests/fillSquare.test.js
@@ -11,8 +11,17 @@ describe('fillSquare()', () => {
   })
   it('can fill an empty 3x3 square with numbers 1-9', () => {
     const square = new Array(3).map((row) => new Array(3).fill(null))
-    const expected = oneToNine
     const actual = fillSquare(square)
-    expect(actual.flat()).toEqual(expect.arrayContaining(expected))
+    expect(actual.flat()).toEqual(expect.arrayContaining(oneToNine))
+  })
+  it('can fill a 3x3 square already containing a one in the middle', () => {
+    const square = [
+      [null, null, null],
+      [null, 1, null],
+      [null, null, null],
+    ]
+    const actual = fillSquare(square)
+    expect(actual[1][1]).toBe(1)
+    expect(actual.flat()).toEqual(expect.arrayContaining(oneToNine))
   })
 })
